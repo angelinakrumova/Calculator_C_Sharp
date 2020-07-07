9faf0bf78a8d28f6009a83847dc7029a15422e84
angelinakrumova <51401045+angelinakrumova@users.noreply.github.com>
2020-07-07 21:09:43 +0300

    Code optimization

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,182 +1,144 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Calculator
 {
     /// <summary>
-    /// doubleeraction logic for MainWindow.xaml
+    /// floateraction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        double a;
-        string operation;
+        private float a;
+        private float num;
+        private Operations op;
+        private Button btn;
+
+        private enum Operations
+        {
+            ADDITION,
+            SUBTRACTION,
+            MULTIPLICATION,
+            DIVISION
+        }
 
         public MainWindow()
         {
             InitializeComponent();
         }
         
-        private void addNum(double n)
+        //updates the equation textboxes 
+        private void addNum(float n)
         {
             calculations_Txt.Text += n;
+            equation_Txt.Text += n;
         }
 
-        // operations
+        //special buttons
         private void btn_C(object sender, RoutedEventArgs e)
         {
             calculations_Txt.Text = "";
+            equation_Txt.Text = "";
             a = 0;
         }
-
-        private void btn_devide(object sender, RoutedEventArgs e)
-        {
-            operation = "/";
-            a = Convert.ToDouble(calculations_Txt.Text);
-             calculations_Txt.Text = "";
-        }
-
-        private void btn_substract(object sender, RoutedEventArgs e)
-        {
-            
-            operation = "-";
-            a = Convert.ToDouble(calculations_Txt.Text);
-             calculations_Txt.Text = "";
-        }
-
-        private void btn_sum(object sender, RoutedEventArgs e)
-        {
-            
-            operation = "+";
-            a = Convert.ToDouble(calculations_Txt.Text);
-             calculations_Txt.Text = "";
-        }
-
-        private void btn_multiply(object sender, RoutedEventArgs e)
-        {
-            
-            operation = "*";
-            a = Convert.ToDouble(calculations_Txt.Text);
-             calculations_Txt.Text = "";
-        }
-
-        private void btn_dott(object sender, RoutedEventArgs e)
+        private void btn_dot(object sender, RoutedEventArgs e)
         {
             calculations_Txt.Text += ".";
+            equation_Txt.Text += ".";
         }
 
-        //enter numbers:
-        double num;
-        private void btn_7(object sender, RoutedEventArgs e)
+        // Operations
+        private void arithmeticOperations(object sender, RoutedEventArgs e)
         {
-            num = 7;
-            addNum(num);
+            a = float.Parse(calculations_Txt.Text);
+            calculations_Txt.Text = "";
+
+            if (sender is Button)
+            {
+                btn = (Button)sender;
+
+                if (btn.Name.Equals("addition")){
+                    op = Operations.ADDITION;
+                    equation_Txt.Text += " + ";
+                }
+                if (btn.Name.Equals("sutbraction"))
+                {
+                    op = Operations.SUBTRACTION;
+                    equation_Txt.Text += " - ";
+                }
+                if (btn.Name.Equals("multiplication"))
+                {
+                    op = Operations.MULTIPLICATION;
+                    equation_Txt.Text += " * ";
+                }
+                if (btn.Name.Equals("division"))
+                {
+                    op = Operations.DIVISION;
+                    equation_Txt.Text += " / ";
+                }
+            }
         }
 
-        private void btn_8(object sender, RoutedEventArgs e)
+        //enter numbers
+        private void EnterNumbers(object sender, RoutedEventArgs e)
         {
-            num = 8;
-            addNum(num);
+            if (sender is Button)
+            {
+                btn = (Button)sender;
+                for (int i = 0; i < 10; i++)
+                {
+                    if (btn.Name.Contains(i.ToString()))
+                    {
+                        num = i;
+                        addNum(i);
+                    }
+                }
+            }
         }
-
-        private void btn_9(object sender, RoutedEventArgs e)
-        {
-            num = 9;
-            addNum(num);
-        }
-
-        private void btn_4(object sender, RoutedEventArgs e)
-        {
-            num = 4;
-            addNum(num);
-        }
-
-        private void btn_5(object sender, RoutedEventArgs e)
-        {
-            num = 5;
-            addNum(num);
-        }
-
-        private void btn_6(object sender, RoutedEventArgs e)
-        {
-            num = 6;
-            addNum(num);
-        }
-
-        private void btn_1(object sender, RoutedEventArgs e)
-        {
-            num = 1;
-            addNum(num);
-        }
-
-        private void btn_2(object sender, RoutedEventArgs e)
-        {
-            num = 2;
-            addNum(num);
-        }
-
-        private void btn_3(object sender, RoutedEventArgs e)
-        {
-            num = 3;
-            addNum(num);
-        }
-
-        private void btn_0(object sender, RoutedEventArgs e)
-        {
-            num = 0;
-            addNum(num);
-        }
-
+        
         //calculate and print result
         private void btn_equal(object sender, RoutedEventArgs e)
         {
-            double result;
-            double b;
-
-            b = Convert.ToDouble(calculations_Txt.Text);
-
-            if (operation == "+")
+            float result;
+            float b;
+            int i = (int)op;
+            b = float.Parse(calculations_Txt.Text);
+            equation_Txt.Text += " = ";
+            switch (i)
             {
-                result = (a + b);
-                calculations_Txt.Text = Convert.ToString(result);
-                a = b;
-            }
-            if (operation == "-")
-            {
-                result = (a - b);
-                calculations_Txt.Text = Convert.ToString(result);
-                a = b;
-            }
-            if (operation == "*")
-            {
-                result = (a * b);
-                calculations_Txt.Text = Convert.ToString(result);
-                a = b;
-            }
-            if (operation == "/")
-            {
-                if (b == 0)
-                {
-                    calculations_Txt.Text = "Cannot divide by zero";
-
-                }
-                else
-                {
-                    result = (a / b);
+                case 0: //sum
+                    result = (a + b);
                     calculations_Txt.Text = Convert.ToString(result);
                     a = b;
-                }
+                    equation_Txt.Text += result + "";
+                    break;
+                case 1: //subtract
+                    result = (a - b);
+                    calculations_Txt.Text = Convert.ToString(result);
+                    a = b;
+                    equation_Txt.Text += result + "";
+                    break;
+                case 2: //multiplicate
+                    result = (a * b);
+                    calculations_Txt.Text = Convert.ToString(result);
+                    a = b;
+                    equation_Txt.Text += result + "";
+                    break;
+                case 3: //divide
+                    if (b == 0)
+                    {
+                        calculations_Txt.Text = "Cannot divide by zero";
+                        equation_Txt.Text += "X";
+                    }
+                    else
+                    {
+                        result = (a / b);
+                        calculations_Txt.Text = Convert.ToString(result);
+                        a = b;
+                        equation_Txt.Text += result + "";
+                    }
+                    break;
+                default: break;
             }
         }
     }
